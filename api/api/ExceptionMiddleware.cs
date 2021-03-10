a800eff3f29d4b212d2ab9a3f9212d3219f0870d
Eduardo Tortato de Araujo Bastos <etortato@gmail.com>
2021-03-10 14:02:19 -0300

    Adding an error handling to format error messages.

---
+++ api/api/ExceptionMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace api
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate next;
+        public ExceptionMiddleware(RequestDelegate requestDelegate) { next = requestDelegate; }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                httpContext.Response.ContentType = "application/text";
+                httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await httpContext.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+}
